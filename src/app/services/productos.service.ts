d8c54e6355a2d275596fc9dc58795013f31874de
josmig <pantigosomiguel59@gmail.com>
2021-02-28 22:54:52 -0500

    prepara firebase para productos - ingreso de productos - services - firebase

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,34 @@
+import { Producto } from './../interfaces/producto.interface';
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  producto : Producto[] = [];
+
+
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos(){
+    /* https://angular-html-webapp-5a215-default-rtdb.firebaseio.com/productos_idx.json */
+    /* this.http.get(`https://angular-html-webapp-5a215-default-rtdb.firebaseio.com/${url}`) */
+    this.http.get('https://angular-html-webapp-5a215-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe((data: Producto[] ) => {
+        console.log(data);
+        this.producto= data;
+        setTimeout(() => {
+          this.cargando = false;
+        },2000);
+      })
+  }
+
+
+}
